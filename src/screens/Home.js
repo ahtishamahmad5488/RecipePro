9a9c27376e94824741a148885e73845963537a63
Ahtisham Ahmad <ahtisham548849@gmail.com>
2024-08-29 15:25:49 +0500

    Home Top View Create

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,42 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image, StatusBar} from 'react-native';
 import React from 'react';
+import {deviceWidth} from '../components/Dimensions';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <StatusBar barStyle={'light-content'} />
+      <View style={styles.topView}>
+        <Image
+          style={styles.banner}
+          source={require('../images/home/home.jpg')}
+        />
+        <View style={styles.transparentView}></View>
+      </View>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  topView: {
+    width: deviceWidth,
+    height: '40%',
+  },
+  banner: {
+    width: '100%',
+    height: '100%',
+  },
+  transparentView: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
